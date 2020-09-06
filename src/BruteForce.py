8591d4cde9ba898286d5fa9e1569a97f952df312
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-09-06 23:28:40 +0200

    Abandon first optimization:
    * qualitative experiments showed that using them almost doubles the runtime

--- src/BruteForce.py
+++ src/BruteForce.py
@@ -103,14 +103,6 @@ def check_alg_for_root_comp(root_comp, words, comps):
 # Recursively checks all possible decision trees with a given root-value in a Divide and Conquer approach.
 # Returns 'True' if a correct decision tree was found.
 def check_alg(current_node, words, comps, prev_comps, first_rel_char):
-    # If only one word is left from previous comparisons we can immediately decide for this words r-value
-    if len(words) <= 1:
-        return True
-
-    # If only one distinct r-value is left for all words, we can immediatly take this r-value
-    if len(set([w_r for (w, w_r) in words])) == 1:
-        return True
-
     comparisons_left = m - current_node.depth
     subword_length_left = n - first_rel_char
 
